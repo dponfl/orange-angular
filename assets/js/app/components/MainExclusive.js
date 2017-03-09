7841b376fdae8f531c54551dd089577fa4e6b014
dmsch <dschukin@mail.ru>
2017-03-09 22:45:00 +0200

    nice small panels display in groups by 3 and other improvements

--- assets/js/app/components/MainExclusive.js
+++ assets/js/app/components/MainExclusive.js
@@ -2,8 +2,72 @@
 
 (function () {
   function MainExclusiveCtrl() {
-    this.testing = 123;
+    this.panelGroups = [];
+    this.innerGroup = [];
     this.panels = [
+      {
+        hit: true,
+        img: {
+          href: '../../images/30593_700x500.jpg',
+          dataLightbox: '1',
+          dataTitle: 'Комментарий к фото',
+          src: '../../images/30593_700x500.jpg'
+        },
+        content: [
+          { label: 'Адрес:',
+            text: 'Lorem ipsum dolor sit amet.'},
+          { label: 'Тип объекта:',
+            text: 'Lorem ipsum'},
+          { label: 'Кол. комнат:',
+            text: '3'},
+          { label: 'Тип сделки:',
+            text: 'Lorem ipsum dolor sit amet.'},
+          { label: 'Стоимость:',
+            text: 'Lorem ipsum dolor'}
+        ]
+      },
+      {
+        hit: false,
+        img: {
+          href: '../../images/30593_700x500.jpg',
+          dataLightbox: '2',
+          dataTitle: 'Комментарий к фото',
+          src: '../../images/30593_700x500.jpg'
+        },
+        content: [
+          { label: 'Адрес:',
+            text: '11111111111111 11111111111 1111111111 111111111111 1111111111111 111111111'},
+          { label: 'Тип объекта:',
+            text: '222222222'},
+          { label: 'Кол. комнат:',
+            text: '3'},
+          { label: 'Тип сделки:',
+            text: '33333333333'},
+          { label: 'Стоимость:',
+            text: '444444444444'}
+        ]
+      },
+      {
+        hit: true,
+        img: {
+          href: '../../images/30593_700x500.jpg',
+          dataLightbox: '3',
+          dataTitle: 'Комментарий к фото',
+          src: '../../images/30593_700x500.jpg'
+        },
+        content: [
+          { label: 'Адрес:',
+            text: '55555555555'},
+          { label: 'Тип объекта:',
+            text: '7777777777777'},
+          /*{ label: 'Кол. комнат:',
+            text: '3'},
+          { label: 'Тип сделки:',
+            text: '888888888888.'},
+          { label: 'Стоимость:',
+            text: '99999999999999'}*/
+        ]
+      },
       {
         hit: true,
         img: {
@@ -46,6 +110,7 @@
             text: '444444444444'}
         ]
       },
+/*
       {
         hit: true,
         img: {
@@ -60,25 +125,33 @@
           { label: 'Тип объекта:',
             text: '7777777777777'},
           { label: 'Кол. комнат:',
-            text: '3'},
-          { label: 'Тип сделки:',
-            text: '888888888888.'},
-          { label: 'Стоимость:',
-            text: '99999999999999'}
+           text: '3'},
+           { label: 'Тип сделки:',
+           text: '888888888888.'},
+           { label: 'Стоимость:',
+           text: '99999999999999'}
         ]
       }
-    ]
+*/
+    ];
+    for (let i = 1; i < this.panels.length+1; i++) {
+      this.innerGroup.push(this.panels[i-1]);
+      if (i % 3 == 0) {
+        this.panelGroups.push(this.innerGroup);
+        this.innerGroup = [];
+      }
+    }
+    if (this.innerGroup.length != 0) this.panelGroups.push(this.innerGroup);
   }
   
   angular.module('Orange')
     .controller('MainExclusiveCtrl', MainExclusiveCtrl)
-    .component('mainExclusivePanel', {
+    .component('smallPanel', {
       bindings: {
         hit: '<',
         img: '<',
         content: '<'
       },
-      // controller: 'MainExclusiveCtrl as excl',
       templateUrl: 'templates/view/main/exclusivePanel.html'
     })
 })();
