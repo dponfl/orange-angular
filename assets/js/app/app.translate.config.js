478986ec9080ae8ecbe3c760b4816bf2517ca9a5
dmsch <dschukin@mail.ru>
2017-03-19 17:59:41 +0200

    nav localization

--- assets/js/app/app.translate.config.js
+++ assets/js/app/app.translate.config.js
@@ -7,12 +7,50 @@ translateConfig.$inject = ['$translateProvider'];
 /* @ngInject */
 function translateConfig ($translateProvider) {
   $translateProvider.translations('en', {
+
+    // Main menu items
     'NAV_HOME': 'Home',
+    'NAV_EXCLUSIVE': 'Exclusive',
+    'NAV_DAILY': 'Daily',
+    'NAV_LONG_TERM': 'Long term',
+    'NAV_SALES': 'Sales',
+    'NAV_QA': 'Q&A',
+    'NAV_SERVICES': 'Services',
+    'NAV_CONTACTS': 'Contacts',
+    'NAV_REQ_BUTTON': 'Make request',
+
+    // Request button dropdown menu items
+    'NAV_REQ_DAILY': 'Daily rent',
+    'NAV_REQ_LONG_TERM': 'Long term rent',
+    'NAV_REQ_LET_PROP': 'Let property',
+    'NAV_REQ_BUY_PROP': 'Buy property',
+    'NAV_REQ_SELL_PROP': 'Sell property',
+
+    // General form items
     'GEN_CITY': 'City'
   });
 
   $translateProvider.translations('ru', {
+
+    // Main menu items
     'NAV_HOME': 'Главная',
+    'NAV_EXCLUSIVE': 'Эксклюзив',
+    'NAV_DAILY': 'Посуточная аренда',
+    'NAV_LONG_TERM': 'Долгосрочная аренда',
+    'NAV_SALES': 'Продажа',
+    'NAV_QA': 'Вопросы',
+    'NAV_SERVICES': 'Доп. услуги',
+    'NAV_CONTACTS': 'Контакты',
+    'NAV_REQ_BUTTON': 'Оставить заявку',
+
+    // Request button dropdown menu items
+    'NAV_REQ_DAILY': 'Снять недв. посуточно',
+    'NAV_REQ_LONG_TERM': 'Снять недв. долгосрочно',
+    'NAV_REQ_LET_PROP': 'Сдать недвижимость',
+    'NAV_REQ_BUY_PROP': 'Купить недвижимость',
+    'NAV_REQ_SELL_PROP': 'Продать недвижимость',
+
+    // General form items
     'GEN_CITY': 'Город'
   });
 
