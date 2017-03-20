57589d39b68d8277a8e8ccbc9fecd9748600d41c
dmsch <dschukin@mail.ru>
2017-03-20 23:55:06 +0200

    important update of filter form plus some investigations in watch

--- assets/js/app/components/exclusive.filters.js
+++ assets/js/app/components/exclusive.filters.js
@@ -7,32 +7,82 @@
     .component('exclusiveFilter', {
       controller: 'ExclusiveFiltersCtrl',
       bindings: {
-        ttt: '<',
-        dealList: '<',
-        objList: '<',
-        cityList: '<',
-        roomList: '<'
+        dealList: '=',
+        objList: '=',
+        cityList: '=',
+        roomList: '='
       },
       templateUrl: 'templates/view/exclusive/filter.html'
     });
 
-  ExclusiveFiltersCtrl.$inject = ['GeneralConfigService'];
+  ExclusiveFiltersCtrl.$inject = ['GeneralConfigService', '$log', '$scope'];
 
   /* @ngInject */
-  function ExclusiveFiltersCtrl(GeneralConfigService) {
+  function ExclusiveFiltersCtrl(GeneralConfigService, $log, $scope) {
     var vm = this;
+
+    vm.lang = GeneralConfigService.orangeConfig.lang;
 
     vm.dealList = GeneralConfigService.orangeConfig.dealList[GeneralConfigService.orangeConfig.lang];
     vm.objList = GeneralConfigService.orangeConfig.objList[GeneralConfigService.orangeConfig.lang];
     vm.cityList = GeneralConfigService.orangeConfig.cityList[GeneralConfigService.orangeConfig.lang];
     vm.roomList = GeneralConfigService.orangeConfig.roomList[GeneralConfigService.orangeConfig.lang];
 
+    vm.initData = {
+      deal: GeneralConfigService.orangeConfig.dealList[GeneralConfigService.orangeConfig.lang][0],
+      obj: vm.objList[0],
+      city: vm.cityList[0],
+      room: vm.roomList[0],
+      objNum: ''
+    };
+
+    // vm.formData = vm.initData;
+
+    vm.formData = {};
+
+    vm.formData.deal = vm.initData.deal;
+    vm.formData.obj = vm.initData.obj;
+    vm.formData.city = vm.initData.city;
+    vm.formData.room = vm.initData.room;
+    vm.formData.objNum = vm.initData.objNum;
+
+    $scope.$watch('ExclusiveFiltersCtrl.lang', function (newVal, oldVal) {
+      $log.info('newVal: ' + newVal);
+      $log.info('oldVal: ' +oldVal);
+    });
+
+    vm.test = function (newVal, oldVal) {
+      $log.info('newVal: ' + newVal);
+      $log.info('oldVal: ' +oldVal);
+    };
+
+    vm.update = function () {
+      $log.info('vm.update...');
+      vm.dealList = GeneralConfigService.orangeConfig.dealList[GeneralConfigService.orangeConfig.lang];
+    };
+
     vm.find = function () {
-      console.log('Find...');
+      $log.info('Find...');
+      $log.debug(vm.formData);
     };
 
     vm.clear = function () {
-      console.log('Clear...')
+      $log.info('Clear...');
+      $log.warn('GeneralConfigService.orangeConfig.lang: ' + GeneralConfigService.orangeConfig.lang);
+      vm.initData = {
+        deal: vm.dealList[0],
+        obj: vm.objList[0],
+        city: vm.cityList[0],
+        room: vm.roomList[0],
+        objNum: ''
+      };
+
+      vm.dealList = GeneralConfigService.orangeConfig.dealList[GeneralConfigService.orangeConfig.lang];
+      vm.formData.deal = GeneralConfigService.orangeConfig.dealList[GeneralConfigService.orangeConfig.lang][0];
+      vm.formData.obj = vm.initData.obj;
+      vm.formData.city = vm.initData.city;
+      vm.formData.room = vm.initData.room;
+      vm.formData.objNum = vm.initData.objNum;
     };
   }
 
