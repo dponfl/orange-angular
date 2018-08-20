9896589a8456294fa41d692f8e679492a730c0f9
dmsch <dschukin@mail.ru>
2018-08-20 11:42:09 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,6 @@
 
 echo "Deploy bash script..."
 pwd
-#echo "Going to install webstudiopro-sails-auth..."
-#cd node_modules/webstudiopro-sails-auth/
-#pwd
-#echo "Going to activate webstudiopro-sails-auth..."
-#npm i
-#cd ../..
-#pwd
 echo "Going to install run bower i"
 bower i
 rm -rf ./assets/vendor/angular-animate/
