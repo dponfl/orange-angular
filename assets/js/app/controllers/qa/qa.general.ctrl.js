e40f0c8a8beb9a48fe0c24e893490fba846acbeb
dmsch <dschukin@mail.ru>
2018-11-03 16:06:49 +0200

    faq section

--- assets/js/app/controllers/qa/qa.general.ctrl.js
+++ assets/js/app/controllers/qa/qa.general.ctrl.js
@@ -4,10 +4,11 @@
   angular.module('OrangeClient')
     .controller('QAGeneralCtrl',QAGeneralCtrl);
 
-  QAGeneralCtrl.$inject = ['$rootScope', '$log'];
-  function QAGeneralCtrl($rootScope, $log) {
+  QAGeneralCtrl.$inject = ['$rootScope', '$log', '$sce', 'lodash'];
+  function QAGeneralCtrl($rootScope, $log, $sce, lodash) {
 
     var vm = this;
+    var _ = lodash;
 
     vm.useLang = $rootScope.lang;
     vm.home = {};
@@ -18,12 +19,26 @@
 
     function _update() {
 
+      var useBody = [];
+
       vm.useLang = $rootScope.lang;
+
+      $log.info('$rootScope.orangeConfig.contentQA[vm.useLang].body:');
+      $log.info($rootScope.orangeConfig.contentQA[vm.useLang].body);
+
+      _.forEach($rootScope.orangeConfig.contentQA[vm.useLang].body, function (val) {
+        useBody.push({
+          q: val.q,
+          a: $sce.trustAsHtml(val.a)
+        });
+      });
+
 
       vm.qa = {
         title: $rootScope.orangeConfig.contentQA[vm.useLang].title || '',
-        body: $rootScope.orangeConfig.contentQA[vm.useLang].body || [],
+        body: useBody,
       };
+
 
       // $log.info("QAGeneralCtrl, vm.qa:");
       // $log.info(vm.qa);
